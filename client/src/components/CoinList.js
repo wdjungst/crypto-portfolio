9635940330484a3ff04570c920b6d6398290582a
David Jungst <djungst@gmail.com>
2018-10-08 11:34:57 -0600

    coin list

--- client/src/components/CoinList.js
+++ client/src/components/CoinList.js
@@ -1,6 +1,71 @@
 import React from 'react'
+import { connect } from 'react-redux'
+import { 
+  Table,
+  Divider,
+} from 'semantic-ui-react'
+import { Link } from 'react-router-dom'
+import { getCoins } from '../reducers/coins'
 
-const CoinList = () => null
+class CoinList extends React.Component {
+  componentDidMount() {
+    this.props.dispatch(getCoins())
+  }
 
-export default CoinList
+  headers = () => {
+    return ['Symbol', 'Name', 'Price'].map( header =>
+      <Table.HeaderCell key={header}>{header}</Table.HeaderCell>
+    )
+  }
+
+  render() {
+    const { coins } = this.props
+    return (
+      <>
+        <Divider />
+        <Table celled striped>
+          <Table.Header>
+            { this.headers() }
+          </Table.Header>
+          <Table.Body>
+            { coins.map( coin => {
+                const { name, price, symbol, cmc_id, id } = coin
+                return (
+                  <Table.Row key={id}>
+                    <Table.Cell>
+                      <Link to={`/coins/${cmc_id}`}>
+                        { symbol }
+                      </Link>
+                    </Table.Cell>
+                    <Table.Cell>
+                      { name }
+                    </Table.Cell>
+                    <Table.Cell>
+                      ${ parseFloat(price).toFixed(2) }
+                    </Table.Cell>
+                  </Table.Row>
+                )
+              })
+            }
+          </Table.Body>
+
+        </Table>
+      </>
+    )
+  }
+}
+
+const mapStateToProps = (state) => {
+  return { coins: state.coins }
+}
+
+
+export default connect(mapStateToProps)(CoinList)
+
+
+
+
+
+
+
 
