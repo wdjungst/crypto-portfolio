1ec83835cae09f89b684f78b914e73d3166e4caf
David Jungst <djungst@gmail.com>
2018-10-08 09:46:51 -0600

    initial commit

---
+++ app/models/user.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+  include DeviseTokenAuth::Concerns::User
+
+  has_many :watched_coins, dependent: :destroy
+  has_many :coins, through: :watched_coins
+end
